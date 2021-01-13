1ae9f9b941c092ff3a88ef17aa87a9cb0a4ba105
pvsreejith <pvs1980@gmail.com>
2021-01-13 14:40:10 +0800

    head

--- WindowsFormsApp5/Program.cs
+++ WindowsFormsApp5/Program.cs
@@ -11,6 +11,10 @@ namespace WindowsFormsApp5
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
+/// <summary>
+        /// The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
